bbfde88eae875b453e77af09033f6f79a2bea4bc
Yana Makarova <=yana.mcarova@gmail.com>
2021-06-30 22:33:29 +0300

    added second homework

---
+++ homework2/script.js
@@ -0,0 +1,12 @@
+let userAge = 17;
+let userName = 'Daniella';
+let userSurname = 'Hardy';
+
+let newUser;
+let shoppingList;
+let userStatus;
+let employeeSalary;
+let cursorCoordinates;
+
+console.log('Hello World!');
+console.log('userName', userName);
