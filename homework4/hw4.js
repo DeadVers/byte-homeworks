6eb9efd72c1b9951795cdc4f1412b9aa4b86220b
Yana Makarova <=yana.mcarova@gmail.com>
2021-07-07 20:36:15 +0300

    added homework 4

---
+++ homework4/hw4.js
@@ -0,0 +1,48 @@
+// task 1
+
+// function countSum(a, b){
+//     let sum = a + b;
+//     return sum
+// }
+
+// let getSum = (a, b) => a + b;
+
+// res1 = countSum(12, 12);
+// res2 = countSum(true, true);
+
+// result1 = getSum(3, 6);
+// result2 = getSum('Cyber', 'Punk');
+
+// console.log(res1);
+// console.log(res2);
+// console.log(result1);
+// console.log(result2);
+
+// task 2
+
+// let userName = prompt('Как Вас зовут?');
+// let userAge = Number(prompt('Сколько Вам лет?'));
+// greeting();
+
+// function greeting() {
+//     if (userAge > 30){
+//         let olderGreeting = 'Здравствуйте, '+ userName;
+//         alert( olderGreeting);
+//     } else {
+//         let youngerGreeting = 'Привет, ' + userName;
+//         alert(youngerGreeting);
+//     }
+// }
+
+// task 3
+
+// function toPower(a, b = 2){
+//     expon = a ** b;
+//     return expon;
+// }
+
+// result = toPower();
+// console.log(result);
+
+
+
