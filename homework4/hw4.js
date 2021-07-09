dd6ec51e8c516e5c655157e6c531241e0599660d
Yana Makarova <=yana.mcarova@gmail.com>
2021-07-09 16:31:54 +0300

    edited homework4

--- homework4/hw4.js
+++ homework4/hw4.js
@@ -1,48 +1,50 @@
 // task 1
 
-// function countSum(a, b){
-//     let sum = a + b;
-//     return sum
-// }
+function countSum(a, b){
+    const sum = a + b;
+    return sum
+}
 
-// let getSum = (a, b) => a + b;
+const getSum = (a, b) => a + b;
 
-// res1 = countSum(12, 12);
-// res2 = countSum(true, true);
+const res1 = countSum(12, 12);
+const res2 = countSum(true, true);
 
-// result1 = getSum(3, 6);
-// result2 = getSum('Cyber', 'Punk');
+const result1 = getSum(3, 6);
+const result2 = getSum('Cyber', 'Punk');
 
-// console.log(res1);
-// console.log(res2);
-// console.log(result1);
-// console.log(result2);
+console.log(res1);
+console.log(res2);
+console.log(result1);
+console.log(result2);
 
 // task 2
 
-// let userName = prompt('Как Вас зовут?');
-// let userAge = Number(prompt('Сколько Вам лет?'));
-// greeting();
 
-// function greeting() {
-//     if (userAge > 30){
-//         let olderGreeting = 'Здравствуйте, '+ userName;
-//         alert( olderGreeting);
-//     } else {
-//         let youngerGreeting = 'Привет, ' + userName;
-//         alert(youngerGreeting);
-//     }
-// }
+greeting();
+
+function greeting() {
+    const userName = prompt('Как Вас зовут?');
+    const userAge = Number(prompt('Сколько Вам лет?'));
+
+    if (userAge > 30){
+        const olderGreeting = 'Здравствуйте, '+ userName;
+        alert(olderGreeting);
+    } else {
+        const youngerGreeting = 'Привет, ' + userName;
+        alert(youngerGreeting);
+    } 
+}
 
 // task 3
 
-// function toPower(a, b = 2){
-//     expon = a ** b;
-//     return expon;
-// }
+function toPower(a, b = 2){
+    const expon = a ** b;
+    return expon;
+}
 
-// result = toPower();
-// console.log(result);
+const result = toPower();
+console.log(result);
 
 
 
