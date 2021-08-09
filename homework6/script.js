402f3523b220c57af972ce4f8aa7f6be71fb4690
Yana Makarova <=yana.mcarova@gmail.com>
2021-08-09 22:33:46 +0300

    edited hw6

--- homework6/script.js
+++ homework6/script.js
@@ -1,65 +1,71 @@
    // TASK 1
 
 
-// const getFullName = () => {
-//     let name;
-//     let surname;
-//     let password;
-//     let editedFullName;
+const getFullName = () => {
+    let name;
+    let surname;
+    let password;
 
-//     do{
-//         name = prompt('name?');
-//         if(name.length < 1){
-//             alert('enter valid name')
-//         }
-//     }while(!name || name.length < 1)
+    do{
+        name = prompt('name?');
+        if(name.length < 1){
+            alert('enter valid name')
+        }
+    }while(!name)
 
-//     do{
-//         surname = prompt('surname?');
-//         if(surname.length < 1){
-//             alert('enter valid surname')
-//         }
-//     }while(!surname || surname.length < 1)
+    do{
+        surname = prompt('surname?');
+        if(surname.length < 1){
+            alert('enter valid surname')
+        }
+    }while(!surname)
 
-//     do{
-//         password = prompt('password?');
-//         if(  password.length < 6 
-//           || password.localeCompare(password.toUpperCase()) === 0 
-//           || password.localeCompare(password.toLowerCase()) === 0){
-//               alert('enter valid password')
-//           }
-//     }while(!password 
-//         || password.length < 6 
-//         || password.localeCompare(password.toUpperCase()) === 0 
-//         || password.localeCompare(password.toLowerCase()) === 0)
-    
-//     editedFullName = `${name.slice(0,1).toUpperCase()}${name.slice(1,).toLowerCase()} ${surname.slice(0,1).toUpperCase()}${surname.slice(1,).toLowerCase()}`
-//     alert(editedFullName);
+    const isValidPassword = () =>{
+        if( password.length < 6 
+            || password.localeCompare(password.toUpperCase()) === 0 
+            || password.localeCompare(password.toLowerCase()) === 0){
+                return false
+            }
+    }
 
-//     console.log(` Name: ${name}\n Surname: ${surname}\n Password: ${password} \n Editted Full Name: ${editedFullName} `);
+    do{
+        password = prompt('password?');
+        if(isValidPassword() === false){
+              alert('enter valid password')
+          }
+    }while(!password 
+        || isValidPassword() === false)
 
-//     return ` Name: ${name}\n Surname: ${surname}\n Password: ${password} \n Editted Full Name: ${editedFullName} `
-// } 
+    const formattedText = (word) => {
+        return word[0].toUpperCase() + word.slice(1,).toLowerCase();
+    }
+
+    const editedFullName = formattedText(name)+ ' ' + formattedText(surname);
+    alert(editedFullName);
+
+    console.log(` Name: ${name}\n Surname: ${surname}\n Password: ${password} \n Editted Full Name: ${editedFullName} `);
+    return ` Name: ${name}\n Surname: ${surname}\n Password: ${password} \n Editted Full Name: ${editedFullName} `
+} 
 
 
-// getFullName();
+getFullName();
 
 // проверочные значения:
-// QwErTyS
-// qwertys
-// QWERTYS
-// QwEr
+// QwErTyS - ок
+// qwertys - !ок
+// QWERTYS - !ок
+// QwEr -  !ок
 
    
 
     // TASK 2
 
-const a = Number(prompt('Введите начало промежутка:'));
-const b = Number(prompt('Введите конец промежутка:'));
+// const a = Number(prompt('Введите начало промежутка:'));
+// const b = Number(prompt('Введите конец промежутка:'));
 
-const numberGenerator = (num1, num2) =>{
-    return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
-}
+// const numberGenerator = (num1, num2) =>{
+//     return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+// }
 
-result = numberGenerator(a, b);
-console.log(result);
+// result = numberGenerator(a, b);
+// console.log(result);
