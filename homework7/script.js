1b49b9d60e306105afded4d936dd8365b63a101b
Yana Makarova <=yana.mcarova@gmail.com>
2021-08-09 22:50:46 +0300

    edited hw7

--- homework7/script.js
+++ homework7/script.js
@@ -1,47 +1,48 @@
      // TASK 1
 
-// const makeSchedule = () => {
-//     const schedule = {};
-//     do{
-//        const askTime = prompt('Когда?');
+const makeSchedule = () => {
+    const schedule = {};
+    do{
+       const askTime = prompt('Когда?');
 
-//        if(askTime === null ){
-//            break;
-//        }
-//        const askTask = prompt('Какая задача?');
+       if(askTime === null ){
+           break;
+       }
+       const askTask = prompt('Какая задача?');
 
-//        if( askTask === null){
-//             break;
-//        }
+       if( askTask === null){
+            break;
+       }
 
        
-//        schedule[askTime] = askTask;
+       schedule[askTime] = askTask;
        
-//        console.log(schedule);
-//     }while(true)
-// }
+       console.log(schedule);
+    }while(true)
+    return schedule;
+}
 
-// makeSchedule();
+makeSchedule();
 
     // TASK 2
 
-    const salaries = {
-        John: "4300.00",
-        Ann: "5700.40",
-        Pete: "4900.95",
-    };
+    // const salaries = {
+    //     John: "4300.00",
+    //     Ann: "5700.40",
+    //     Pete: "4900.95",
+    // };
 
-    const salarySum = (object) => {
-        let sum = 0;
-        for (let key in object){
-            // console.log(Number(object[key]));
+    // const salarySum = (object) => {
+    //     let sum = 0;
+    //     for (let key in object){
+    //         // console.log(Number(object[key]));
             
-            sum += Number(object[key]);
-            result = sum.toFixed(2);
-        }
-        console.log(result);
-        return result;
+    //         sum += Number(object[key]);
+    //         result = sum.toFixed(2);
+    //     }
+    //     console.log(result);
+    //     return result;
         
-    }
+    // }
 
-    salarySum(salaries);
+    // salarySum(salaries);
